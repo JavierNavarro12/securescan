ba3883e8346de5b5d29b0efc514bb44f8cfbc36d
JavierNavarro12 <navarrojavi107@gmail.com>
2026-01-14 19:59:19 +0100

    Allow embedding in portfolio

--- next.config.js
+++ next.config.js
@@ -15,11 +15,7 @@ const nextConfig = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://js.stripe.com; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; font-src 'self' data:; connect-src 'self' https://api.stripe.com https://*.supabase.co wss://*.supabase.co; frame-src 'self' https://js.stripe.com https://hooks.stripe.com;",
-          },
-          {
-            key: 'X-Frame-Options',
-            value: 'DENY',
+            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://js.stripe.com; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; font-src 'self' data:; connect-src 'self' https://api.stripe.com https://*.supabase.co wss://*.supabase.co; frame-src 'self' https://js.stripe.com https://hooks.stripe.com; frame-ancestors 'self' http://localhost:3000 https://portfoliojaviernavarro.netlify.app;",
           },
           {
             key: 'X-Content-Type-Options',
